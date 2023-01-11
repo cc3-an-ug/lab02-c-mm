d9c87bcb38d62643999db56177a99ae9315cf83d
Andrés Castellanos <me@andrescv.dev>
2023-01-10 19:02:10 -0600

    chore: initial commit

---
+++ ex1/flip_bit.c
@@ -0,0 +1,8 @@
+#include "bit_ops.h"
+
+
+// Flip the nth bit of the value of x.
+// Assume 0 <= n <= 31
+void flip_bit(unsigned * x, unsigned n) {
+  // YOUR CODE HERE
+}
---
+++ ex2/lfsr_calculate.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+
+void lfsr_calculate(uint16_t *reg) {
+  /* YOUR CODE HERE */
+}
